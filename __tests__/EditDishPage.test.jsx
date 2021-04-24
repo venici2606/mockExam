9e103f05025d695720ccd48c37e278999113d27f
venici2606 <venicia_p@live.no>
2021-04-24 22:16:39 +0200

    EditDishPage can show loading screen (refactored)

--- __tests__/EditDishPage.test.jsx
+++ __tests__/EditDishPage.test.jsx
@@ -1,35 +1,39 @@
-import {EditDishPage} from "../src/client/EditDishPage";
+import { EditDishPage } from "../src/client/EditDishPage";
 import React from "react";
 import ReactDOM from "react-dom";
-import {MemoryRouter} from "react-router";
-import {act} from "react-dom/test-utils";
+import { MemoryRouter } from "react-router";
+import { act } from "react-dom/test-utils";
+
+async function renderForTest(child) {
+  const container = document.createElement("div");
+  await act(async () => {
+    await ReactDOM.render(<MemoryRouter>{child}</MemoryRouter>, container);
+  });
+  return container;
+}
 
 describe("edit dish page", () => {
-    it("can show information about an existing dish", async () => {
-        const getDish = () => ({
-            name: "Pizza",
-            price: "119"
-        });
-        const container = document.createElement("div");
-        await act(async () => {
-            await ReactDOM.render(<MemoryRouter> <EditDishPage dishApi={{getDish }}/> </MemoryRouter>, container);
-        });
-        expect(container.innerHTML).toMatchSnapshot();
-        expect(container.querySelector("h1").textContent).toEqual("Edit existing dish (Pizza)");
+  it("can show information about an existing dish", async () => {
+    const getDish = () => ({
+      name: "Pizza",
+      price: "119",
     });
+    const container = await renderForTest(
+      <EditDishPage dishApi={{ getDish }} />
+    );
+    expect(container.innerHTML).toMatchSnapshot();
+    expect(container.querySelector("h1").textContent).toEqual(
+      "Edit existing dish (Pizza)"
+    );
+  });
 
+  it("can show loading screen", async () => {
+    const getDish = () => new Promise((resolve) => {});
 
-
-
-
-    it("can show loading screen", async () => {
-        const getDish = () => new Promise((resolve) => {});
-        const container = document.createElement("div");
-        await act(async () => {
-            await ReactDOM.render(<MemoryRouter> <EditDishPage dishApi={{getDish }}/> </MemoryRouter>, container);
-        });
-        expect(container.innerHTML).toMatchSnapshot();
-        expect(container.querySelector("div").textContent).toEqual("Loading ...");
-    });
-
+    const container = await renderForTest(
+      <EditDishPage dishApi={{ getDish }} />
+    );
+    expect(container.innerHTML).toMatchSnapshot();
+    expect(container.querySelector("div").textContent).toEqual("Loading ...");
+  });
 });
