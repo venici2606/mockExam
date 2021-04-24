0f687f0ceb1a98e3727e9ccdf89a390222416ee7
venici2606 <venicia_p@live.no>
2021-04-24 15:38:17 +0200

    Add ifrst test (LoadingView)

---
+++ __tests__/LoadingView.test.jsx
@@ -0,0 +1,16 @@
+import {LoadingView} from "../src/client/components/LoadingView";
+import React from "react";
+import ReactDOM from "react-dom";
+import {act} from "react-dom/test-utils";
+
+describe("loading view", () => {
+    it("show loading view on dom", () => {
+        const container = document.createElement("div");
+        document.body.appendChild(container);
+        act(() => {
+            ReactDOM.render(<LoadingView />, container);
+        });
+
+        expect(container.innerHTML).toMatchSnapshot();
+    });
+});
