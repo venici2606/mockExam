5b8f781c0859802d1eb19bf9a148d1ed24bc9645
venici2606 <venicia_p@live.no>
2021-04-24 18:15:26 +0200

    EditDishPage shows snapshot of edit form

---
+++ __tests__/EditDishPage.test.jsx
@@ -0,0 +1,23 @@
+import {EditDishPage} from "../src/client/EditDishPage";
+import React from "react";
+import ReactDOM from "react-dom";
+import {MemoryRouter} from "react-router";
+import {act} from "react-dom/test-utils";
+
+describe("edit dish page", () => {
+    it("can show information about an existing dish", async () => {
+        const getDish = () => ({
+            name: "Pizza",
+            price: "119"
+        });
+        const container = document.createElement("div");
+        await act(async () => {
+            await ReactDOM.render(<MemoryRouter> <EditDishPage dishApi={{getDish }}/> </MemoryRouter>, container);
+        });
+        expect(container.innerHTML).toMatchSnapshot();
+        expect(container.querySelector("h1").textContent).toEqual("Edit existing dish (Pizza)");
+    });
+
+
+
+});
