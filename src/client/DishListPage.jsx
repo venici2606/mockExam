923fe4b9de4fd2d946744b2d43b87d0a54685a0e
LaurentBaj <laurent_baj@hotmail.com>
2021-04-25 13:53:02 +0200

    Server Code Refac

--- src/client/DishListPage.jsx
+++ src/client/DishListPage.jsx
@@ -9,16 +9,10 @@ export function DishListPage({ dishApi, loadProfile, msg }) {
     async () => await dishApi.listDishes()
   );
 
-  const message = msg;
   const { data } = useLoading(async () => await loadProfile());
 
-  if (error) {
-    return <ErrorView error={error} reload={reload} />;
-  }
-
-  if (loading || !dishes) {
-    return <LoadingView />;
-  }
+  if (error) return <ErrorView error={error} reload={reload} />
+  if (loading || !dishes) return <LoadingView />
 
   /* If login add link */
   function output(data, name, id, price) {
@@ -30,31 +24,14 @@ export function DishListPage({ dishApi, loadProfile, msg }) {
       );
     else return `Dish ${id}: ${name} ${price} kr`;
   }
-
-  /*  return (
-    <>
-      <h1>Menu</h1>
-      {dishes.map(({ id, name, price }) => (
-        <li key={id}>
-          {/!*          <Link to={`/dishes/${id}/edit`}>
-            Dish {id}: {name} {price} kr
-          </Link>*!/}
-          {output(data, name, id, price)}
-        </li>
-      ))}
-      <p>
-        <i>You can click on the dishes to change them!</i>
-      </p>
-    </>
-  );*/
-
+  
   return (
     <div>
       <h3>Menu</h3>
       {dishes.map(({ id, name, price }) => (
         <li key={id}>{output(data, name, id, price)}</li>
       ))}
-      <p>{message}</p>
+      <p>{msg}</p>
     </div>
   );
 }
--- src/server/server.js
+++ src/server/server.js
@@ -3,50 +3,18 @@ const path = require("path");
 const fetch = require("node-fetch");
 const bodyParser = require("body-parser");
 
+const dishApi = require("./dishApi");
+
+
+
 const app = express();
 
-const dishes = [
-  {
-    id: 1,
-    name: "Cheeseburger",
-    price: "239",
-  },
-  {
-    id: 2,
-    name: "Pizza",
-    price: "119",
-  },
-];
 app.use(bodyParser.json());
 app.use(express.static(path.resolve(__dirname, "..", "..", "dist")));
+app.use("/api/dishes", dishApi);
 
-app.get("/api/dishes", (req, res) => {
-  console.log(dishes);
-  res.json(dishes);
-});
 
-app.get("/api/dishes/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  const dish = dishes.find((b) => b.id === id);
-  console.log({ dish });
-  res.json(dish);
-});
-
-app.put("/api/dishes/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  const dishIndex = dishes.findIndex((b) => b.id === id);
-  const { name, price } = req.body;
-  dishes[dishIndex] = { name, price, id };
-  res.status(200).end();
-});
-
-app.post("/api/dishes", (req, res) => {
-  const { name, price } = req.body;
-  console.log(req.body);
-  dishes.push({ name, price, id: dishes.length + 1 });
-  res.status(201).end();
-});
-
+// Google Login
 const discoveryURL =
   "https://accounts.google.com/.well-known/openid-configuration";
 
@@ -78,6 +46,9 @@ app.get("/api/profile", async (req, res) => {
   return res.json(req.userinfo);
 });
 
+
+
+// Handle Server
 app.use((req, res, next) => {
   if (req.method === "GET" && !req.path.startsWith("/api")) {
     return res.sendFile(
