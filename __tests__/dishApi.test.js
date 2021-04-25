d10ab7bb83a4aa455af872c4bd16690ccc39ca05
venici2606 <venicia_p@live.no>
2021-04-25 17:17:28 +0200

    Added tests for dishApi

--- __tests__/dishApi.test.js
+++ __tests__/dishApi.test.js
@@ -5,29 +5,47 @@ const app = express();
 app.use(require("body-parser").json());
 app.use(require("../src/server/dishApi"));
 
-describe("dish API", () => {
-  it("can return the predefined dishes", async () => {
+describe("dishes api", () => {
+  it("can list initial dishes", async () => {
     await request(app)
-      .get("")
+      .get("/")
       .then((response) => {
         expect(response.body.find(({ id }) => id === 2)).toMatchObject({
           name: "Pizza",
+          price: "119",
         });
       });
   });
 
-  it("can create a new dish", async () => {
+  it("can add a new dish", async () => {
     await request(app)
-      .post("")
+      .post("/")
       .send({
-        name: "Fish",
-        price: "200",
+        name: "New dish",
+        price: 100,
       })
       .expect(201);
     await request(app)
-      .get("")
+      .get("/")
       .then((response) => {
-        expect(response.body.map(({ name }) => name)).toContain("Fish");
+        expect(response.body.map(({ name }) => name)).toContain("New dish");
+      });
+  });
+
+  it("can update existing dishes", async () => {
+    const book = (await request(app).get("/2")).body;
+    const updated = {
+      ...book,
+      price: "99",
+    };
+    await request(app).put("/2").send(updated).expect(200);
+    await request(app)
+      .get("/2")
+      .then((response) => {
+        expect(response.body).toMatchObject({
+          id: 2,
+          price: "99",
+        });
       });
   });
 });
