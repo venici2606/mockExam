1da22e1a1acf2d36838bad895564cc105e76b66c
venici2606 <venicia_p@live.no>
2021-04-25 14:19:44 +0200

    Supertest of API POST /api/dishes

--- __tests__/dishApi.test.js
+++ __tests__/dishApi.test.js
@@ -2,6 +2,7 @@ const request = require("supertest");
 const express = require("express");
 
 const app = express();
+app.use(require("body-parser").json());
 app.use(require("../src/server/dishApi"));
 
 describe("dish API", () => {
@@ -12,6 +13,21 @@ describe("dish API", () => {
         expect(response.body.find(({ id }) => id === 2)).toMatchObject({
           name: "Pizza",
         });
+      });
+  });
+
+  it("can create a new dish", async () => {
+    await request(app)
+      .post("")
+      .send({
+        name: "Fish",
+        price: "200",
+      })
+      .expect(201);
+    await request(app)
+      .get("")
+      .then((response) => {
+        expect(response.body.map(({ name }) => name)).toContain("Fish");
       });
   });
 });
